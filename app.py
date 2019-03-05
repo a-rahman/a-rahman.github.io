e8124e911e9fd793f1d2ef56dcfb21b73f46f767
Abrar Rahman <abrar.a.rahman@gmail.com>
2019-03-04 22:15:22 -0500

    Get minimum of the site running on flask

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, Response, send_file
+import sys
+#sys.path.insert(0, "/VGM-Piano-Composer/")
+from VGM_Piano_Composer.predict import generate
+app = Flask(__name__)
+import time
+
+
+@app.route('/')
+def static_page():
+    return render_template('index.html')
+
+@app.route('/default.mid')
+def default():
+    t0 = time.time()
+    model_file, note_file, song_length = r'VGM_Piano_Composer/weights.hdf5', r'VGM_Piano_Composer/data/notes', 500 
+    midi_stream = generate(model_file, note_file, song_length)
+    midi_stream.write('midi', fp='temp.mid')
+    temp = open('temp.mid', 'rb')
+    print("It took {} to run".format(time.time()-t0))
+    return send_file(temp, mimetype='audio/midi')
+
+if __name__ == '__main__':
+    app.run()
+    
