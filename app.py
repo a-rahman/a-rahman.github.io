7c5bd90a63b31e4309779116153878ef5a122ff7
Abrar Rahman <abrar.rahman@sri.com>
2019-03-18 15:58:14 -0400

    Add functions for other models, experiment with different accordion type

--- app.py
+++ app.py
@@ -20,6 +20,26 @@ def default():
     print("It took {} to run".format(time.time()-t0))
     return send_file(temp, mimetype='audio/midi')
 
+@app.route('/ffvii.mid')
+def ffvii():
+    t0 = time.time()
+    model_file, note_file, song_length = r'VGM_Piano_Composer/weights.hdf5', r'VGM_Piano_Composer/data/notes', 500 
+    midi_stream = generate(model_file, note_file, song_length)
+    midi_stream.write('midi', fp='ff7_temp.mid')
+    temp = open('ff7_temp.mid', 'rb')
+    print("It took {} to run".format(time.time()-t0))
+    return send_file(temp, mimetype='audio/midi')
+
+@app.route('/ocarina.mid')
+def ocarina():
+    t0 = time.time()
+    model_file, note_file, song_length = r'VGM_Piano_Composer/weights.hdf5', r'VGM_Piano_Composer/data/notes', 500 
+    midi_stream = generate(model_file, note_file, song_length)
+    midi_stream.write('midi', fp='oot_temp.mid')
+    temp = open('oot_temp.mid', 'rb')
+    print("It took {} to run".format(time.time()-t0))
+    return send_file(temp, mimetype='audio/midi')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
     
