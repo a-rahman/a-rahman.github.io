072445f0d595d9d0af32a845f0babeba2bc683cf
Abrar Rahman <abrar.a.rahman@gmail.com>
2019-03-26 08:00:31 -0400

    Use  pre-generated midi instead of generating on the spot

--- app.py
+++ app.py
@@ -4,40 +4,27 @@ import sys
 from VGM_Piano_Composer.predict import generate
 app = Flask(__name__)
 import time
+import glob
+import random
+
+
+ff7_files = glob.glob(r'ff7_output/*.mid')
+oot_files = glob.glob(r'oot_output/*.mid')
 
 
 @app.route('/')
 def static_page():
     return render_template('index.html')
 
-@app.route('/ffx.mid')
-def default():
-    t0 = time.time()
-    model_file, note_file, song_length = r'VGM_Piano_Composer/ffx.hdf5', r'VGM_Piano_Composer/data/ffx', 125 
-    midi_stream = generate(model_file, note_file, song_length)
-    midi_stream.write('midi', fp='ffx_temp.mid')
-    temp = open('ffx_temp.mid', 'rb')
-    print("It took {} to run".format(time.time()-t0))
-    return Response(temp, mimetype='audio/midi')
 
 @app.route('/ffvii.mid')
 def ffvii():
-    t0 = time.time()
-    model_file, note_file, song_length = r'VGM_Piano_Composer/ff7.hdf5', r'VGM_Piano_Composer/data/ff7', 125 
-    midi_stream = generate(model_file, note_file, song_length)
-    midi_stream.write('midi', fp='ff7_temp.mid')
-    temp = open('ff7_temp.mid', 'rb')
-    print("It took {} to run".format(time.time()-t0))
+    temp = open(random.choice(ff7_files), 'rb')
     return Response(temp, mimetype='audio/midi')
 
 @app.route('/ocarina.mid')
 def ocarina():
-    t0 = time.time()
-    model_file, note_file, song_length = r'VGM_Piano_Composer/ocarina.hdf5', r'VGM_Piano_Composer/data/ocarina', 125 
-    midi_stream = generate(model_file, note_file, song_length)
-    midi_stream.write('midi', fp='oot_temp.mid')
-    temp = open('oot_temp.mid', 'rb')
-    print("It took {} to run".format(time.time()-t0))
+    temp = open(random.choice(oot_files), 'rb')
     return Response(temp, mimetype='audio/midi')
 
 if __name__ == '__main__':
